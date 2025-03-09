7d16b1c5954b5a72abac31d1bb020c40f1593186
m-essam-s <sir.m.essam.g@gmail.com>
2025-03-09 05:49:12 +0200

    OverLoading

---
+++ STLab1/src/main/java/com/mycompany/stlab1/STLab1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.stlab1;
+
+/**
+ *
+ * @author SIRME
+ */
+public class STLab1 {
+
+    int x = 10;
+    int y = 50;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello, Dark");
+
+        STLab1 obj = new STLab1();
+
+        System.out.println(obj.x);
+    }
+}
